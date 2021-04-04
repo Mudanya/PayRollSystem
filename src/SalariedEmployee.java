02be27aabd736ac7bb17d7c3ddfc7c21794ca370
Mudanya <nmudanya94@gmail.com>
2021-04-05 00:24:24 +0300

    Added salaried employee class

---
+++ src/SalariedEmployee.java
@@ -0,0 +1,30 @@
+public class SalariedEmployee extends Employee {
+    private double weeklySalary;
+
+    public SalariedEmployee( String first, String last, String ssn, double salary ) {
+        super(first, last, ssn);
+        setWeeklySalary(salary);
+    }
+
+    public void setWeeklySalary( double weekly ) {
+        if( weekly >= 0.0 )
+            weeklySalary = weekly;
+        else throw new IllegalArgumentException(
+                "Salary must me be greater than 0.0 ");
+    }
+
+    public double getWeeklySalary() {
+        return weeklySalary;
+    }
+
+    @Override
+    public double earnings() {
+        return getWeeklySalary();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Salaried employee:%s\n%s: $%,.2f",
+                "weekly salary", getWeeklySalary());
+    }
+}
